474873cafd80be62961695f2253161d10da9da7e
japtomtelecom <japtomtelecom@gmail.com>
2026-09-22 15:19:25 -0400

    correcion visual

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -135,7 +135,7 @@ export default function AppShell({ children }) {
           🚪 Cerrar sesión
         </button>
       </aside>
-      <main className="flex-1 p-4 md:p-8 max-w-6xl mx-auto w-full">{children}</main>
+      <main className="flex-1 p-4 md:p-8 w-full">{children}</main>
     </div>
   );
 }
